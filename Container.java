e44314b272b7772b086484680d5f1f574fa49f4c
deepthireddy98 <33576626+deepthireddy98@users.noreply.github.com>
2018-01-21 19:45:47 +0530

    Create Container.java

---
+++ Container.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Container
+{
+
+    private int containerId;
+	   private int containerAge;
+   	public Container(int contId, int contAge)
+	{
+		
+       containerId = contId;
+		containerAge = contAge;
+	}
+	public int getContainerId()
+	{
+		return this.containerId;	
+	}
+	public int getContainerAge()
+	{
+		return containerAge;
+	}
+}
